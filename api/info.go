275404c8faf8af7fbf7438a9001d82d28c2f1e10
Francisco Omar <omarrp@gmail.com>
2021-01-07 15:24:11 -0300

    Update function

--- api/info.go
+++ api/info.go
@@ -12,16 +12,16 @@ func Info(w http.ResponseWriter, r *http.Request) {
 	nc := runtime.NumCPU()
 	ng := runtime.NumGoroutine()
 	// ncc := runtime.NumCgoCall()
-	gmp := runtime.GOMAXPROCS()
+	// gmp := runtime.GOMAXPROCS()
 	os := runtime.GOOS
 	arch := runtime.GOARCH
 	info := map[string]string{
 		"NumCPU":       strconv.Itoa(nc),
 		"NumGoroutine": strconv.Itoa(ng),
 		// "NumCgoCall":   strconv.Itoa(ncc),
-		"GOMAXPROCS": strconv.Itoa(gmp),
-		"OS":         os,
-		"ARCH":       arch,
+		// "GOMAXPROCS": strconv.Itoa(gmp),
+		"OS":   os,
+		"ARCH": arch,
 	}
 	for k, v := range info {
 		fmt.Fprintf(w, k, v)
