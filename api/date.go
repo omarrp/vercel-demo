c3bd38f6d654825466a2c730d8c7d7cf30b4f1a5
Francisco Omar <omarrp@gmail.com>
2021-01-07 14:55:05 -0300

    Update date function for use RFC 3339/ISO 8601 format

--- api/date.go
+++ api/date.go
@@ -8,6 +8,6 @@ import (
 
 // Date basic serveless function
 func Date(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format("Mon Jan 2 15:04:05 MST 2006")
+	currentTime := time.Now().Format(time.RFC3339)
 	fmt.Fprintf(w, currentTime)
 }
