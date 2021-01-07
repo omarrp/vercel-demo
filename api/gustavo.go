97a8b8840d38c92e039e5a588a8758c300e626ba
Francisco Omar <omarrp@gmail.com>
2021-01-07 15:53:38 -0300

    Teste com Gustavo II

--- api/gustavo.go
+++ api/gustavo.go
@@ -2,17 +2,16 @@ package demo
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
 // Gustavo teste com gustavo
 func Gustavo(w http.ResponseWriter, r *http.Request) {
 	// defer resp.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Fprintf(w, err.Error())
-		return
-	}
+	// body, err := ioutil.ReadAll(r.Body)
+	// if err != nil {
+	// 	fmt.Fprintf(w, err.Error())
+	// 	return
+	// }
 	fmt.Fprintf(w, r.URL.Path)
 }
